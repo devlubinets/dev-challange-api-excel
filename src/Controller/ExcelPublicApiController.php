cee38c19484e8673cfb5de650160e16327909d06
Lubynets Kyrylo <dev.lubinets@gmail.com>
2023-09-28 20:11:18 +0100

    added final implementation for api

--- src/Controller/ExcelPublicApiController.php
+++ src/Controller/ExcelPublicApiController.php
@@ -5,10 +5,31 @@ namespace App\Controller;
 use App\DTO\Sheet1;
 use App\DTO\Sheet2;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExcelPublicApiController extends API
 {
+    #[Route('/api/' . self::API_VERSION . '/{sheetId}/{cellId}', name: 'app_excel_public_api123', requirements: [
+        'sheetId' => '\d+',
+    ], methods: ['POST'])]
+    public function upsert(Request $request, int $sheetId, $cellId): JsonResponse
+    {
+        $sheet = match ($sheetId) {
+            1 => Sheet1::data,
+            2 => Sheet2::data,
+            default => $this->json([], 422),
+        };
+
+        $variable = json_decode($request->getContent(), true)["value"];
+        $sheet[$cellId] = $this->calc($variable, $sheet);
+
+        return $this->json([
+            "value" => $sheet[$cellId],
+            "result" =>  $sheet[$cellId]
+        ], 201);
+    }
+
     #[Route('/api/' . self::API_VERSION . '/{sheetId}', name: 'app_excel_public_api2', requirements: [
         'sheetId' => '\d+',
     ], methods: ['GET'])]
@@ -37,5 +58,25 @@ class ExcelPublicApiController extends API
         }
 
         return $this->json([$value]);
+    }
+
+    /**
+     * @param string $variable
+     * @param $sheet
+     * @return int
+     */
+    protected function calc(string $variable, $sheet): int
+    {
+        $operation = "";
+        $cls = "";
+        preg_match_all("#(\w+)#", $variable, $cls);
+        preg_match_all('#[+\-*/]#', $variable, $operation);
+
+        return match ($operation[0][0]) {
+            "+" => $sheet[$cls[0][0]]["value"] + $sheet[$cls[0][1]]["value"],
+            "-" => $sheet[$cls[0][0]]["value"] - $sheet[$cls[0][1]]["value"],
+            "/" => $sheet[$cls[0][0]]["value"] / $sheet[$cls[0][1]]["value"],
+            "*" => $sheet[$cls[0][0]]["value"] * $sheet[$cls[0][1]]["value"],
+        };
     }
 }
